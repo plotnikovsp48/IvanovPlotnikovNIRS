8497e56ed244c0deb2b878a4e454cbc8533b4f93
Roman_Ivanov <junktown@bk.ru>
2015-10-16 10:36:11 +0300

    made pretty header

---
+++ nirs/nirs/header_main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+
+#include <opencv2\core\core.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+#endif
--- nirs/nirs/main.cpp
+++ nirs/nirs/main.cpp
@@ -1,11 +1,4 @@
-#include <opencv2\core\core.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
-#include <opencv2\highgui\highgui.hpp>
-
-#include <iostream>
-
-using namespace std;
-using namespace cv;
+#include "header_main.h"
 
 int main()
 {
